5008c55b9142eabbafd66694b02f3708b482dd68
Eduardo do Prado Cardoso <dpradoeduardo@gmail.com>
2021-09-11 14:47:36 -0300

    update room

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,7 @@
+import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+
+import { database } from '../services/firebase'
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -13,6 +16,7 @@ import { useAuth } from '../hooks.ts/useAuth';
 export function Home(){
     const history = useHistory(); 
     const { user, signInWithGoogle } = useAuth();
+    const [roomCode, setRoomCode] = useState('');
 
 
     async function handleCreateRoom(){
@@ -22,6 +26,23 @@ export function Home(){
 
 
         history.push('/rooms/new')
+    }
+
+    async function handleJoinRoom(event: FormEvent){
+        event.preventDefault();
+
+        if(roomCode.trim() === '' ){
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if(!roomRef.exists()) {
+            alert('Room does not exist.')
+            return;
+        }
+
+        history.push(`/rooms/${roomCode}`)
     }
 
     return(
@@ -39,9 +60,11 @@ export function Home(){
                     Crie sua sala com o Google
                 </button>
                 <div className="separator">ou entre em uma sala</div>
-                <form>
+                <form onSubmit={handleJoinRoom}>
                     <input type="text"
                         placeholder="Digite o código da sala"
+                        onChange={event => setRoomCode(event.target.value)}
+                        value={roomCode}
                     />
                     <Button type="submit">
                         Entrar na sala
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,33 @@
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+import '../styles/room.scss'
+
 export function Room () {
     return (
-        <h1>SALAS</h1>
+        <div id="page-room">
+            <header>
+                <div className="content">
+               <img src={logoImg} alt="LetMeAsk" />
+                    <div>codigo</div>
+                </div>
+
+            </header>
+
+            <main>
+                <div className="room-title">
+                    <h1>Sala React</h1>
+                    <span>4 perguntas</span>
+
+                </div>
+                <form>
+                    <textarea placeholder="O que você quer perguntar"/>
+                
+                <div className="form-footer"> 
+                <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>]
+                <Button type="submit"> Enviar pergunta </Button>
+                </div>
+                </form>
+            </main>
+        </div>
     );
 }
