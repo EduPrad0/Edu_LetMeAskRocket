c06ce78b353de2aac87829fbe580f1072e09ad05
Eduardo do Prado Cardoso <dpradoeduardo@gmail.com>
2021-09-12 18:19:10 -0300

    update noS

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,14 +1,52 @@
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
+import { RoomCode } from '../components/RoomCode';
+import { useAuth } from '../hooks.ts/useAuth';
 import '../styles/room.scss'
 
+type RoomParams = {
+    id: string;
+}
+
 export function Room () {
+    const { user } = useAuth();
+    const params = useParams<RoomParams>();
+    const [newQuestion, setNewQuestion] = useState('');
+  
+  
+    const roomId = params.id;
+
+    async function handleSendQuestion(){
+        if(newQuestion.trim() == ''){
+            return;
+        }
+
+        if(!user){
+            throw new Error('You must be logged in')
+        }
+
+        const question = {
+            content: newQuestion,
+            author: {
+                nome : user.nome, 
+                avatar: user.avatar,
+            },
+            isHighlighted: false,
+            isAnswered: false
+        }
+
+
+
+    }
+
     return (
         <div id="page-room">
-            <header>
+            <header>   
                 <div className="content">
                <img src={logoImg} alt="LetMeAsk" />
-                    <div>codigo</div>
+                <RoomCode code={roomId}/>
                 </div>
 
             </header>
@@ -20,7 +58,11 @@ export function Room () {
 
                 </div>
                 <form>
-                    <textarea placeholder="O que você quer perguntar"/>
+                    <textarea 
+                    placeholder="O que você quer perguntar"
+                    onChange={event => setNewQuestion(event.target.value)}
+                    value ={newQuestion}
+                    />
                 
                 <div className="form-footer"> 
                 <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
